da1fe7c09b37c65caa27de3422d06d4aa66b303c
Dat Vu <dothanhdat.vu@mail.com>
2016-09-11 16:44:46 +1000

    added thumbs up icon to counter

--- public/js/components/install-counter.js
+++ public/js/components/install-counter.js
@@ -4,6 +4,7 @@ var InstallCounter = React.createClass({
       <div>
         <h4>Number of installs</h4>
         <div id="counter">
+          <i className="fa fa-thumbs-o-up"></i>
           <span>{this.props.data}</span>
         </div>
       </div>
