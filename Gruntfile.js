cca7ccbe45b372101e1979f46c65243d7d6e2f7d
Dat Vu <dothanhdat.vu@mail.com>
2016-09-11 12:48:54 +1000

    installed Grunt to handle automated tasks

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+  grunt.registerTask('watch', [ 'watch' ]);
+  grunt.registerTask('default', [
+    'concat:scripts',
+    'less'
+  ]);
+  grunt.registerTask('serve', [
+    'concat:scripts',
+    'less',
+    'watch'
+  ]);
+
+  grunt.initConfig({
+    concat: {
+      scripts: {
+        src: [
+          'public/js/components/*.js',
+          'public/js/app.js',
+        ],
+        dest: 'public/js/scripts.js'
+      },
+    },
+    less: {
+      style: {
+        files: {
+          "public/css/styles.css": "css/styles.less"
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['public/js/app.js', 'public/js/components/*.js'],
+        tasks: ['concat:app'],
+        options: {
+          livereload: true,
+        }
+      },
+      css: {
+        files: ['public/css/*.less', 'public/css/components/*.less'],
+        tasks: ['less:style'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
